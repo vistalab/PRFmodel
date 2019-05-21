de8729487e7060bfeb6d5daaf6722ce4fbcd07ff
Brian Wandell <wandell@stanford.edu>
2019-05-21 16:47:49 -0700

    Planning a first stimulus generation script

---
+++ scripts/s_pmStimulus.m
@@ -0,0 +1,12 @@
+% s_pmStimulus
+%
+% Experimenting with stimulus generation
+%
+% Path needs:
+%    vistadisp
+%    vistasoft
+%    psychtoolbox    
+%
+% Here is the moving bar stimulus that contains a checkerboard.
+%
+%?????
