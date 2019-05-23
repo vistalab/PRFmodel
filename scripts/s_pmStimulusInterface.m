441edd9a691e4e205443cc70d74481b07b5633c1
Garikoitz <garikoitz@gmail.com>
2019-05-23 06:49:39 -0700

    cleaned the stimuli generation code

--- scripts/s_pmStimulusInterface.m
+++ scripts/s_pmStimulusInterface.m
@@ -24,31 +24,16 @@ stimulusdir = fullfile(pmRootPath,'data');
 % background color
 grayval = uint8(127); 
 
-% whether we want the fixation grid. 0 = no, 1 = yes
-fixationGrid = 1;
-
-% stimulus starts when this key is detected
-triggerkey = 's';          
-
-% full path of image matrix
-% pathImages = fullfile(prfModelPath,wordsDir,imagesName);
-
-
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 expnum      = 103;
 runnum      = 3; 
-% pathMasks = fullfile('./', 'maskimages.mat');
 
 % how many frames we want a refresh to last. default: 15. previously: 4. 
 % the lower the faster
 frameduration = 4;
 
-% directories
-% path to directory that contains the stimulus .mat files
-% stimulusdir = fullfile('./');
-
 %tweaking (in the cni)
-dres    = -.9;              % <dres> (optional) is
+dres    = [];              % <dres> (optional) is
                             %   [A B] where this is the desired resolution to imresize the images to (using bicubic interpolation).
                             %     if supplied, the imresize takes place immediately after loading the images in, and this imresized 
                             %     version is what is cached in the output of this function.
@@ -56,22 +41,12 @@ dres    = -.9;              % <dres> (optional) is
                             %   default is [] which means don't do anything special.  
                             % seems to crash for values >= 1
                             
-
-offset  = [0 -35];        % [X Y] where X and Y are the horizontal and vertical
+offset  = [0 0];        % [X Y] where X and Y are the horizontal and vertical
                            % offsets to apply.  for example, [5 -10] means shift 
                            % 5 pixels to right, shift 10 pixels up.
-     
                            
-% dres    = [];
-% offset  = [0 0];
-
 %%  There is one file that contains many different mask images
-% display. currently not sure when/whether we should specify or define it as empty
-% ptres = [1024 768 60 32]; 
-% OJO CON ESTO: YO ME ASEGURARIA DE PONER LO NUESTRO BIEN, COPIAR DE MI SCRIPT THE MINI
 ptres = [];  % display resolution. [] means to use current display resolution.
-
-
 
 % fixation dot
 fixationinfo = {uint8([255 0 0; 0 0 0; 0 255 0]) 0.5};  % dot colors and alpha value
@@ -81,24 +56,12 @@ changeplusminus = 2;       % plus or minus this amount (in seconds)
 
 % trigger
 tfun = @() fprintf('STIMULUS STARTED.\n');  % function to call once trigger is detected
-
-% tweaking
-% offset = [0 0];
+tfun = [];
 movieflip = [0 0];         % [A B] where A==1 means to flip vertical dimension
                            % and B==1 means to flip horizontal dimension
-
-%%%%%%%%%%%%%%%%%%%%%%%%%% DO NOT EDIT BELOW
-
 % set rand state
 rand('state',sum(100*clock));
 randn('state',sum(100*clock));
-
-% ask the user what to run
-% if ~exist('subjnum','var') 
-%   subjnum = input('What is the subj id? ','s')
-% end
-% expnum = input('What experiment (89=CCW, 90=CW, 91=expand, 92=contract, 93=multibar, 94=wedgeringmash)? ')
-% runnum = input('What run number (for filename)? ')
 
 % prepare inputs
 trialparams = [];
@@ -106,41 +69,35 @@ ptonparams = {ptres,[],0};
 iscolor = 1;
 soafun = @() round(meanchange*(60/frameduration) + changeplusminus*(2*(rand-.5))*(60/frameduration));
 
-% assign depending on whether or not we want fixation grid
-if fixationGrid
-    g = a1.specialoverlay;
-    gscale = imresize(g, -dres);
-    gridImage = gscale;
-else
-    gridImage = [];
-end
-
-% some prep
 if ~exist('images','var')
   images = [];
   maskimages = [];
 end
-% filename = sprintf('%s_subj%d_run%02d_exp%02d.mat',gettimestring,subID,runnum,expnum);
-filename = sprintf('%s_subj%s_run%i_exp%i.mat',gettimestring,subID,runnum,expnum);
-
-
-
+filename = [];
+triggerkey = [];
+gridImage = [];
 [images,maskimages,frameorder] = ...
   pmShowmulticlass(filename,offset,movieflip,frameduration,fixationinfo,...
                  fixationsize,tfun, ...
                  ptonparams,soafun,0,images,expnum,[],grayval,iscolor,...
                  [],[],[],dres,triggerkey, ...
                  [],trialparams,[],maskimages,gridImage,stimulusdir);  
+ images = images{1};            
+ % TEST
+ figure(1);
+ image(images(:,:,:,20) .* maskimages(:,:,2000));colormap gray;axis equal tight off;
              
              
-% Write .avi for visualization
-% Read the mask images (the apertures, they saw words inside the apertures)
+               
+ % {            
+    % Write .avi for visualization
+    % Read the mask images (the apertures, they saw words inside the apertures)
 
-%     params{ns}.scanDuration = 300;
-%     params{ns}.totalScans   = 165;
-%     params{ns}.tr           = 1.82;
-%     config{ns}.config.rescaleStimuliImages = true;
-%     config{ns}.config.imageSideSize = 100;  
+    %     params{ns}.scanDuration = 300;
+    %     params{ns}.totalScans   = 165;
+    %     params{ns}.tr           = 1.82;
+    %     config{ns}.config.rescaleStimuliImages = true;
+    %     config{ns}.config.imageSideSize = 100;  
     
     
 
@@ -160,7 +117,7 @@ filename = sprintf('%s_subj%s_run%i_exp%i.mat',gettimestring,subID,runnum,expnum
     
     
     % If working only with masks maybe we want to resize and binarize
-    wantResize = 0
+    wantResize = 0;
     if wantResize
         temp = zeros(config{ns}.config.imageSideSize, config{ns}.config.imageSideSize, size(stimFileBcbl,3));
         for p=1:size(trAdjImages{ns}, 3)
@@ -174,26 +131,29 @@ filename = sprintf('%s_subj%s_run%i_exp%i.mat',gettimestring,subID,runnum,expnum
     end
     
 
-% We can just work with masks or create the final stimuli multplying the mask
-% with the actual images
-stim = trAdjImages .* trAdjMaskImages;
-% Check:
-% figure(6);image(stim(:,:,1000));colormap gray;axis equal tight off;
+    % We can just work with masks or create the final stimuli multplying the mask
+    % with the actual images
+    stim = trAdjImages .* trAdjMaskImages;
+    % Check:
+    % figure(6);image(stim(:,:,1000));colormap gray;axis equal tight off;
 
-% TODO: 
-% flip the images so that the words read well
+    % TODO: 
+    % flip the images so that the words read well
 
-% Prepare the new file.
-vidObj = VideoWriter(fullfile(pmRootPath,'local','stimuliFlip.avi'));
-open(vidObj);
-% Create an animation.
-axis equal tight off
-set(gca,'nextplot','replacechildren');
-for k = 1:size(stim,3)
-    image(flip(stim(:,:,k))); colormap gray;
-   % Write each frame to the file.
-   currFrame = getframe(gcf);
-   writeVideo(vidObj,currFrame);
-end
-% Close the file.
-close(vidObj);
+
+
+    % Prepare the new file.
+    vidObj = VideoWriter(fullfile(pmRootPath,'local','stimuliFlip.avi'));
+    open(vidObj);
+    % Create an animation.
+    axis equal tight off
+    set(gca,'nextplot','replacechildren');
+    for k = 1:size(stim,3)
+        image(flip(stim(:,:,k))); colormap gray;
+       % Write each frame to the file.
+       currFrame = getframe(gcf);
+       writeVideo(vidObj,currFrame);
+    end
+    % Close the file.
+    close(vidObj);
+%}
