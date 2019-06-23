8714b3f7700134ddd4767873f29966a755a664d0
Garikoitz <garikoitz@gmail.com>
2019-06-22 22:30:46 -0700

    updated the tables to the new prfModel classes

--- functions/tables/pmForwardModelAddRows.m
+++ functions/tables/pmForwardModelAddRows.m
@@ -1,32 +1,49 @@
 function synthDT = pmForwardModelAddRows(synthDT, variableName, values)
-% Adds rows combining the new values with the existing ones. 
-% 
+% Adds rows combining the new values with the existing ones.
+%
 %  Inputs: variable name and value(s)
-% 
+%
 %  Outputs: Matlab table
-% 
-% 
+%
+%
 
-vars = strsplit(variableName,'.');
+
 
 % TODO: add checks to values to validate that the value type corresponds to the
 % variable that we want to expand
 tmp = synthDT;
-for ii=1:length(values)
-    if length(vars) == 1
-        tmp.(vars{1})           = repmat(values(ii), [height(tmp),1]);
-    end
-    if length(vars) == 2
-        tmp.(vars{1}).(vars{2}) = repmat(values(ii), [height(tmp),1]);
-    end
-    if length(vars) == 3
-        tmp.(vars{1}).(vars{2}).(vars{3}) = repmat(values(ii), [height(tmp),1]);
-    end
-    if length(vars) >= 4
-        error('Only implemented until 3 levels of nesting in the table')
-    end
-    synthDT = [synthDT; tmp];
-end
 
+switch variableName
+    case 'HRF'
+        tmp.HRF          = repmat(values, [height(tmp),1]);
+        % Solving the concatenation problem for Type
+        tmp.HRF.Type     = categorical(cellstr(tmp.HRF.Type));
+        synthDT.HRF.Type = categorical(synthDT.HRF.Type);
+        % Solving the concatenation problem for Duration
+        tmp.HRF.Duration     = double(tmp.HRF.Duration);
+        synthDT.HRF.Duration = double(synthDT.HRF.Duration);
+        
+        % Now concatenate the original and the recently created one.
+        synthDT          = [synthDT; tmp];
+    case 'Noise'
+        warning('Noise not implemented yet.')
+    otherwise
+        vars = strsplit(variableName,'.');
+        for ii=1:length(values)
+            if length(vars) == 1
+                tmp.(vars{1})           = repmat(values(ii), [height(tmp),1]);
+            end
+            if length(vars) == 2
+                tmp.(vars{1}).(vars{2}) = repmat(values(ii), [height(tmp),1]);
+            end
+            if length(vars) == 3
+                tmp.(vars{1}).(vars{2}).(vars{3}) = repmat(values(ii), [height(tmp),1]);
+            end
+            if length(vars) >= 4
+                error('Only implemented until 3 levels of nesting in the table')
+            end
+            synthDT = [synthDT; tmp];
+        end
+end
 end
 
--- functions/tables/pmForwardModelTableCreate.m
+++ functions/tables/pmForwardModelTableCreate.m
@@ -32,30 +32,51 @@ synthDT.pm = pm;
 % BEWARE: THIS GROWS VERY FAST: each line multiplyes the rows of the
 % previous one, accumulatively
 
+
+% The behavior is different for HRF and Noise parameters
+
 fieldsToCombine = fieldnames(COMBINE_PARAMETERS);
 for ii=1:length(fieldsToCombine)
     % Construct fieldname
-    fieldName  = fieldsToCombine{ii};
-    fieldValues=getfield(COMBINE_PARAMETERS,fieldName);
-    if ~isstruct(fieldValues)
-        % Add rows with the combinations of parameters we want to check
-        synthDT = pmForwardModelAddRows(synthDT, fieldName,fieldValues);
-    else
-        subFieldsToCombine = fieldnames(fieldValues);
-        for ii=1:length(subFieldsToCombine)
-            % Construct fieldname
-            subFieldName  = subFieldsToCombine{ii};
-            fieldValues   = getfield(COMBINE_PARAMETERS,fieldName,subFieldName);
+    fieldName   = fieldsToCombine{ii};
+    fieldValues = COMBINE_PARAMETERS.(fieldName);
+    switch fieldName
+        case 'HRF'
+            for nh = 1:length(fieldValues)
+                % Add rows with the combinations of parameters we want to check
+                % Check if only some of the fields in params where set
+                % Check the struct params first
+                fieldValues(nh).params = pmParamsCompletenessCheck(fieldValues(nh).params, ...
+                                                   pm.defaultsTable.HRF.params);
+                % Then check that the whole thing should be complete
+                fieldValues(nh)        = pmParamsCompletenessCheck(fieldValues(nh), ...
+                                            table2struct(pm.defaultsTable.HRF));
+                % Convert it to table
+                fieldValuesTable = struct2table(fieldValues(nh),'AsArray',true);
+                % Add all possible combinations based on this options
+                synthDT          = pmForwardModelAddRows(synthDT, fieldName,fieldValuesTable);
+            end
+        case 'Noise'
+        otherwise
             if ~isstruct(fieldValues)
                 % Add rows with the combinations of parameters we want to check
-                subFieldName  = [fieldName '.' subFieldsToCombine{ii}];
-                synthDT = pmForwardModelAddRows(synthDT, subFieldName,fieldValues);
+                synthDT = pmForwardModelAddRows(synthDT, fieldName,fieldValues);
             else
-                error('Only two levels of nesting implemented');
+                subFieldsToCombine = fieldnames(fieldValues);
+                for ii=1:length(subFieldsToCombine)
+                    % Construct fieldname
+                    subFieldName  = subFieldsToCombine{ii};
+                    fieldValues   = getfield(COMBINE_PARAMETERS,fieldName,subFieldName);
+                    if ~isstruct(fieldValues)
+                        % Add rows with the combinations of parameters we want to check
+                        subFieldName  = [fieldName '.' subFieldsToCombine{ii}];
+                        synthDT = pmForwardModelAddRows(synthDT, subFieldName,fieldValues);
+                    else
+                        error('Only two levels of nesting implemented');
+                    end
+                    
+                end
             end
-            
-        end
     end
-    
 end
 
