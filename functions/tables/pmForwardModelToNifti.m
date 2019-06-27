05be02fab2d86a747bf10633116ef4feff511c4a
Garikoitz <garikoitz@gmail.com>
2019-06-27 10:17:21 -0700

    takes the BOLDnoise synthetic signals of every row in the table and creates a 4D nifti file, i.e. volumes and time series, that can be used in other analysis programs

---
+++ functions/tables/pmForwardModelToNifti.m
@@ -0,0 +1,50 @@
+function fname = pmForwardModelToNifti(DT, varargin)
+% Writes BOLDnoise into a nifti file. The size of the nifti file will be related
+% to the size of the table
+% 
+%  Inputs: table with one or several rows of parameters to calculate bold series
+%          in fwd model
+% 
+%  Outputs: returns the same table with the last column of pm-s calculated
+% 
+%  See also: forwardModelToNifti
+% 
+%  GLU Vistalab 2019.05
+
+
+% Read the inputs
+varargin = mrvParamFormat(varargin);
+p = inputParser;
+p.addRequired ('DT', @istable);
+p.addParameter('fname','BOLDnoise.nii.gz', @ischar);
+p.parse(DT,varargin{:});
+
+fname = p.Results.fname;
+
+
+%% Calculate
+pm1    = DT.pm(1);
+Ntime  = pm1.timePointsN;
+Nvoxels = height(DT);
+
+
+dim1 = ceil(sqrt(Nvoxels));
+dim2 = ceil(Nvoxels/dim1);
+
+
+% Create a cube of timeSeries
+myCube = zeros(dim1,dim2,1,Ntime);
+index = 0;
+for ii=1:dim1
+    for jj=1:dim2
+        index = index + 1;
+        if index <= Nvoxels
+            myCube(ii,jj,1,:) =   DT.pm(index).BOLDnoise;
+            assert(DT.pm(index).TR == pm1.TR, 'All BOLDnoise signals TRs should be equal')
+        end
+    end
+end
+% Save it as a nifti
+writeFileNifti(niftiCreate('data', myCube, 'tr', 2, ...
+                            'fname',fname));
+
