67d04d6c4be5582dba5c9badc7f22020cbcdb4ce
noahbenson <nben@nyu.edu>
2019-12-14 12:22:13 -0500

    debugging...

--- gear/prfanalyze/aprf/prfanalyze_aprf.m
+++ gear/prfanalyze/aprf/prfanalyze_aprf.m
@@ -83,7 +83,7 @@ end
 
 %% Write out the results
 estimates_file = fullfile(output_dir, 'estimates.mat');
-estimates = struct(estimates);
+estimates = struct(pmEstimates);
 save(estimates_file, 'estimates', 'pmEstimates');
 results_file = fullfile(output_dir, 'results.mat');
 save(results_file, 'results');
