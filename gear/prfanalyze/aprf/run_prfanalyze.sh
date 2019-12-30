b245875866534847b7a43eee65fdb03fed2dee75
garikoitz <garikoitz@gmail.com>
2019-12-29 21:01:25 -0800

    bug in prfanalyze_aprf.m

--- gear/prfanalyze/aprf/run_prfanalyze.sh
+++ gear/prfanalyze/aprf/run_prfanalyze.sh
@@ -1,1 +1,5 @@
 docker run --rm -it -v "$2":/flywheel/v0/input -v "$3":/flywheel/v0/output garikoitz/prfanalyze-aprf:$1 $4
+docker run --rm -it \
+      -v $2:/flywheel/v0/input \
+      -v $2:/flywheel/v0/output \
+      garikoitz/prfanalyze-aprf:$1 $3 $4
