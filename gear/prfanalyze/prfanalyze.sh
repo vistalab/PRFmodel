a21a20c54bdf3bc49bf7f45150a7e91fd722f7d2
noahbenson <nben@nyu.edu>
2020-01-17 11:55:31 -0500

    verbose flag now works inside docker also

--- gear/prfanalyze/prfanalyze.sh
+++ gear/prfanalyze/prfanalyze.sh
@@ -221,8 +221,12 @@ fi
 ARGS_OUT=("-v" "${OUTPUT}:/flywheel/v0/output")
 if   [ $DEBUG = 1 ]
 then OPTS=("DEBUG" "${OPTS[@]}")
-elif [ $FORCE = 1 ]
-then OPTS=("--force" "${OPTS[@]}")
+else if [ $FORCE = 1 ]
+     then OPTS=("--force" "${OPTS[@]}")
+     fi
+     if [ $VERBOSE = 1 ]
+     then OPTS=("${OPTS[@]}" "--verbose")
+     fi
 fi
 # Put them all together:
 ARGS=("run" "--rm" "-it" "${ARGS_IN[@]}" "${ARGS_OUT[@]}" "$DOCKER" "${OPTS[@]}")
