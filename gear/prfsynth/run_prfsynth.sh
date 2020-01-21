70fa370960204d7f745cddc1aa54ff12dfb0654a
garikoitz <garikoitz@gmail.com>
2020-01-21 08:45:44 -0800

    change order of inputs to be the same to prfanalyze

--- gear/prfsynth/run_prfsynth.sh
+++ gear/prfsynth/run_prfsynth.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker run --rm -it \
-        -v $1:/flywheel/v0/input/config.json \
-        -v $2:/flywheel/v0/output \
+        -v $2:/flywheel/v0/input/config.json \
+        -v $1:/flywheel/v0/output \
         garikoitz/prfsynth:latest $3
