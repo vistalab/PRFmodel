531da089eaecd611258f3a58a4ccda8b0b02ac2d
garikoitz <garikoitz@gmail.com>
2020-01-20 22:18:49 -0800

    copied from afni folder, until Docker and afni is solved, we will need to copy these files and the compiled/ fodler manually

---
+++ gear/prfanalyze/afni-fast/solve.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+set +o verbose   # Command echo off
+
+# If run in debug mode, just exec bash:
+if [ "$1" = "DEBUG" ]
+then exec /bin/bash
+else . /etc/profile.d/conda.sh
+     conda activate base
+fi
+
+# How we print to stdout:
+function note {
+    echo "$CONTAINER" "   " "$*"
+}
+function die {
+    echo "<ERROR>" "$CONTAINER" "   " "$*"
+    exit 1
+}
+
+
+# all we have to do is exec python...
+export PRF_SOLVER="afni"
+MCR_ROOT=/opt/mcr/v95/
+
+export PATH="$PATH:/opt/afni"
+
+time /compiled/run_prfanalyze_afni.sh "$MCR_ROOT" "$4" "$2" "$3" "$5"
+# Check exit status
+[ $? = 0 ] || die "An error occurred during execution of the Matlab executable. Exiting!"
+
+exit 0
