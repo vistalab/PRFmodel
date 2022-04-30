6e9f3ff6705bcc1bea08a590f2965ef313e40f65
Garikoitz <garikoitz@gmail.com>
2022-04-30 16:54:26 +0200

    updated version

--- gear/prfanalyze/vista/build.sh
+++ gear/prfanalyze/vista/build.sh
@@ -2,13 +2,14 @@
 
 [ -z "$USERTAG" ] && USERTAG=garikoitz
 [ -z "$SOLVER"  ] && SOLVER=vista
-[ -z "$VERSION" ] && VERSION=2.1.1
+[ -z "$VERSION" ] && VERSION=2.2.0
 
 # 2.0.5: edited in master, fix in base/run.py for singularity
 # 2.0.8: with the fixed exp
 # 2.0.9: after elines change in css exp range boundaries
 # 2.1.0: same as 2.0.9 but with Matlab 2020b (using base 3.0.0)
 # 2.1.1: same as 2.0.9 but with Matlab 2020b (using base 3.0.1)
+# 2.2.0: includes david's changes to read experimental data from new prfprepare
 
 SCRIPTPATH="$( cd "$(dirname "$0")" && pwd -P )"
 
