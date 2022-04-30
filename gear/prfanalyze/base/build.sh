721dda656107b9ab925d887ba36ee2c12098dddc
Garikoitz <garikoitz@gmail.com>
2022-04-30 16:53:39 +0200

    updated version

--- gear/prfanalyze/base/build.sh
+++ gear/prfanalyze/base/build.sh
@@ -7,10 +7,11 @@
 
 [ -z "$USERTAG" ] && USERTAG=garikoitz
 [ -z "$SOLVER"  ] && SOLVER=base
-[ -z "$VERSION" ] && VERSION=3.0.1
+[ -z "$VERSION" ] && VERSION=3.0.2
 # Versions
 # 3.0.0 first one using matlab 2020b, but maintaining old Python
 # 3.0.1 using the install method with the yml file and conda 3
+# 3.0.2 after merging david changes to use experimental data and prfprepare
 
 SCRIPTPATH="$( cd "$(dirname "$0")" && pwd -P )"
 
@@ -19,5 +20,5 @@ echo "  Path: $SCRIPTPATH"
 echo "------------------------------------------------------------"
 echo ""
 
-docker build --no-cache "$@" --tag "$USERTAG/prfanalyze-$SOLVER:$VERSION" "$SCRIPTPATH"
-# docker build  "$@" --tag "$USERTAG/prfanalyze-$SOLVER:$VERSION" "$SCRIPTPATH"
+# docker build --no-cache "$@" --tag "$USERTAG/prfanalyze-$SOLVER:$VERSION" "$SCRIPTPATH"
+docker build  "$@" --tag "$USERTAG/prfanalyze-$SOLVER:$VERSION" "$SCRIPTPATH"
