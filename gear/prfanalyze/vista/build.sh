a0df7499f0f858108454d03984bdbf4faf3faf26
Sonboth <linhardtdavid@gmail.com>
2022-07-28 10:21:42 +0200

    allow for list of subjects and sessions '[001,002]' or 'all' in config.json, update prfanalyze-vista for new base

--- gear/prfanalyze/vista/build.sh
+++ gear/prfanalyze/vista/build.sh
@@ -2,7 +2,7 @@
 
 [ -z "$USERTAG" ] && USERTAG=garikoitz
 [ -z "$SOLVER"  ] && SOLVER=vista
-[ -z "$VERSION" ] && VERSION=2.2.0
+[ -z "$VERSION" ] && VERSION=2.2.1
 
 # 2.0.5: edited in master, fix in base/run.py for singularity
 # 2.0.8: with the fixed exp
@@ -10,6 +10,7 @@
 # 2.1.0: same as 2.0.9 but with Matlab 2020b (using base 3.0.0)
 # 2.1.1: same as 2.0.9 but with Matlab 2020b (using base 3.0.1)
 # 2.2.0: includes david's changes to read experimental data from new prfprepare
+# 2.2.1: updated base to 3.0.3
 
 SCRIPTPATH="$( cd "$(dirname "$0")" && pwd -P )"
 
