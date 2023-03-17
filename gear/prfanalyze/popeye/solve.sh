fdb844890abb13369d17fc1afbbb8c4c001782f5
Sonboth <linhardtdavid@gmail.com>
2023-03-17 11:25:46 +0100

    source the conda env in the popeye Dockerfile

--- gear/prfanalyze/popeye/solve.sh
+++ gear/prfanalyze/popeye/solve.sh
@@ -2,4 +2,8 @@
 
 # all we have to do is exec python...
 export PRF_SOLVER="popeye"
+
+source /opt/conda/etc/profile.d/conda.sh
+conda activate scientific
+
 exec python /scripts/run_popeye.py "$1" "$2" "$3" "$4" "$5"
