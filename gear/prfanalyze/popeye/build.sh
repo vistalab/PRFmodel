7e1ee8dd5d67d2aa2e46dd5b016de56b8fee9f5b
Sonboth <linhardtdavid@gmail.com>
2023-03-23 14:28:08 +0100

    remove weird -z call

--- gear/prfanalyze/popeye/build.sh
+++ gear/prfanalyze/popeye/build.sh
@@ -1,10 +1,8 @@
 #! /bin/bash
 
-[ -z "$USERTAG" ] && USERTAG=garikoitz
-[ -z "$SOLVER"  ] && SOLVER=popeye
-[ -z "$VERSION" ] && VERSION=2.0.0_3.1.0
-
-# 2.0.0 base 3.1.0
+USERTAG=garikoitz
+SOLVER=popeye
+VERSION=2.0.0_3.1.0
 
 SCRIPTPATH="$( cd "$(dirname "$0")" && pwd -P )"
 
