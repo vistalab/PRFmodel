c156384ec498331bd01d319e168773ddd2d3bbb5
Sonboth <linhardtdavid@gmail.com>
2023-03-24 11:00:04 +0100

    remove double stuff

--- gear/prfanalyze/vista/build.sh
+++ gear/prfanalyze/vista/build.sh
@@ -3,9 +3,6 @@
 USERTAG=garikoitz
 SOLVER=vista
 VERSION=2.2.1_3.1.0
-USERTAG=garikoitz
-SOLVER=vista
-VERSION=2.2.1_3.1.0
 
 # 2.0.5: edited in master, fix in base/run.py for singularity
 # 2.0.8: with the fixed exp
